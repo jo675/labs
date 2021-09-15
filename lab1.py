3b6ba1c6de8ee8c9c02af0ea01c89821a5ffb706
jo675 <johnny.agosile@outlook.com>
2021-09-15 14:39:54 +0200

    Moved print

--- lab1.py
+++ lab1.py
@@ -1,22 +1,22 @@
 import math
 
-#Get input data
-a = int(input("Enter side of cube in cm "))
-b = int(input("Enter side of tetdahedron in cm "))
+# Get input data
+a = float(input("Enter side of cube in cm "))
+b = float(input("Enter side of tetrahedron in cm "))
 
-
-#Calculate volume of cube in cm
+# Calculate volume of cube in cm
 def cube_volume(a):
     volume = a**3
     return round(volume, 2)
 
-get_volume = cube_volume(a)
-print(f'The volume of your cube is {get_volume} cm')
-
+# Calculate volume of tetrahedron in cm
 def tetrahedron_volume(b):
-    # volume = a**3*(math.sqrt(2))
     volume = (b ** 3 / (6 * math.sqrt(2)))
     return round(volume, 2)
 
+# Perform calculations
+get_volume = cube_volume(a)
 get_tetrahedron = tetrahedron_volume(b)
+
+print(f'The volume of your cube is {get_volume} cm')
 print(f'The volume of your tedrahedron is {get_tetrahedron} cm')
