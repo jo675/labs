329a655a79526a4244ac9e45202677af642e8cea
jo675 <johnny.agosile@outlook.com>
2021-09-20 22:43:56 +0200

    Updated functions

--- lab1.py
+++ lab1.py
@@ -1,23 +1,33 @@
 import math
+from typing import Text
 
-# Get input data
-a = float(input("Enter side of cube in cm "))
-b = float(input("Enter side of tetrahedron in cm "))
-
-# Calculate volume of cube in cm
-def cube_volume(a):
+def cube_volume():
+    """
+    Calculate the volume of a cube
+    """
+    a = eval(input("Enter side of cube in cm "))
     volume = a**3
     return round(volume, 2)
 
-# Calculate volume of tetrahedron in cm
-def tetrahedron_volume(b):
+def tetrahedron_volume():
+    """
+    Calculate the volume of a tedrahedron
+    """
+    b = eval(input("Enter side of tetrahedron in cm "))
     volume = (b ** 3 / (6 * math.sqrt(2)))
     return round(volume, 2)
 
-# Get result of calculations
-get_volume = cube_volume(a)
-get_tetrahedron = tetrahedron_volume(b)
-
-# Print result
-print(f'The volume of your cube is {get_volume} cm')
-print(f'The volume of your tedrahedron is {get_tetrahedron} cm')
+while True:
+    print('Press 1 if you want to calculate volume of cube\nPress 2 if you want to calculate volume of tetrahedron\nPress Q if you want to quit\n')
+    x = input('select: ')
+    if x == '1':
+        y = cube_volume()
+        print(f'The volume of your cube is {y} cm^3\n')
+    elif x == '2':
+        z = tetrahedron_volume()
+        print(f'The volume of your tedrahedron is {z} cm^3\n')
+    elif x == 'Q':
+        print('Good bye')
+        break
+    else:
+        print('wrong option')
